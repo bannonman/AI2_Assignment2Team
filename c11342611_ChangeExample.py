df519c6a49aec8a05f6c5e27a1b51430edde7074
bannonman <coreyandrewbannon@gmail.com>
2015-04-06 01:42:49 +0100

    pushing changes to a different file - check it out :D

--- c11342611_ChangeExample.py
+++ c11342611_ChangeExample.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 # read in trainingset data and adding headers : not sure if this is the way to assign the headers as i have hardcoded 1,2,3,4,5,6.... to stop out_of_index error
 columnHeadings=['Id','Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Vertical_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points','Wilderness_Area','1','2','3','Soil_Type','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','Cover_Type']
-Tdata = pd.read_csv("data/testtrainingset.txt",header=None,names=columnHeadings,index_col=False,na_values=['?'],nrows=24)
+Tdata = pd.read_csv("data/trainingset.txt",header=None,names=columnHeadings,index_col=False,na_values=['?'],nrows=6000)
 # Extract Target Feature
 targetLabels = Tdata['Cover_Type']
 # Extract Numeric Descriptive Features
@@ -35,7 +35,7 @@ clf = svm.SVC(gamma=0.001, C=100)
 #Again read above
 clf.fit(train_dfs,targetLabels)
 
-q = {'Elevation':[2516,2516],'Aspect':[23,23],'Slope':[6,6],'Horizontal_Distance_To_Hydrology':[150,150],'Vertical_Distance_To_Hydrology':[4,4],'Horizontal_Distance_To_Roadways':[658,658],'Hillshade_9am':[216,216],'Hillshade_Noon':[227,227],'Hillshade_3pm':[147,147],'Horizontal_Distance_To_Fire_Points':[5541,5541]}
+q = {'Elevation':[2000,2516],'Aspect':[23,23],'Slope':[6,6],'Horizontal_Distance_To_Hydrology':[150,150],'Vertical_Distance_To_Hydrology':[4,4],'Horizontal_Distance_To_Roadways':[658,658],'Hillshade_9am':[216,216],'Hillshade_Noon':[227,227],'Hillshade_3pm':[147,147],'Horizontal_Distance_To_Fire_Points':[5541,5541]}
 col_names = ['Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Vertical_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points']
 qdf = pd.DataFrame.from_dict(q,orient="columns")
 #extract the numeric features
