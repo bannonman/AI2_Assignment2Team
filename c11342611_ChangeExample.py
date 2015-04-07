0ad4393c7f9a00e8f0a8d7dd8c621d837a3f3251
bannonman <coreyandrewbannon@gmail.com>
2015-04-07 20:10:59 +0100

    run

--- c11342611_ChangeExample.py
+++ c11342611_ChangeExample.py
@@ -10,7 +10,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
-
+#print(set(ids[:]))
 # read in trainingset data and adding headers : not sure if this is the way to assign the headers as i have hardcoded 1,2,3,4,5,6.... to stop out_of_index error
 columnHeadings=['Id','Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Vertical_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points','Wilderness_Area','1','2','3','Soil_Type','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','Cover_Type']
 Tdata = pd.read_csv("data/trainingset.txt",header=None,names=columnHeadings,index_col=False,na_values=['?'],nrows=435148)
@@ -50,6 +50,7 @@ cat_dfs= numeric_dfs.T.to_dict().values()
 #print(cat_dfs)
 
 ids = Qdata['Id']
+
 #q = {'Elevation':[2000,2516],'Aspect':[23,23],'Slope':[6,6],'Horizontal_Distance_To_Hydrology':[150,150],'Vertical_Distance_To_Hydrology':[4,4],'Horizontal_Distance_To_Roadways':[658,658],'Hillshade_9am':[216,216],'Hillshade_Noon':[227,227],'Hillshade_3pm':[147,147],'Horizontal_Distance_To_Fire_Points':[5541,5541]}
 col_names = ['Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Vertical_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points']
 qdf = pd.DataFrame.from_dict(list(cat_dfs),orient="columns")
@@ -66,14 +67,12 @@ query = np.hstack((q_num, q_vec_dfs ))
 predictions = decTreeModel.predict(query[:])
 print("Predictions!")
 print("------------------------------")
+print(set(ids[:]))
+#f = open("data/predictions.txt", 'w')
 
-f = open("data/predictions.txt", 'w')
+#for num in range(0,145864):
+   # f.write(ids[num]+','+predictions[num]+'\n')
 
-for num in range(0,145864):
-    f.write(ids[num]+','+predictions[num]+'\n')
 
-#predictList = []
-#predictList.append(ids[:]+":"+predictions)
-#print(predictList)
-print(ids[:]+':'+predictions) 
+#print(ids[:]+':'+predictions) 
 
