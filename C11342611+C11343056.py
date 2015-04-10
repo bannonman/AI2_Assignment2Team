b8c69583a4628cf4fba1640dcc4d4d62ef97fc9a
bannonman <coreyandrebannon@gmail.com>
2015-04-10 13:14:09 +0100

    adding final time changes

--- C11342611+C11343056.py
+++ C11342611+C11343056.py
@@ -11,22 +11,18 @@ __date__ = "10 - 04 - 2015"
 __email__ = "corey.bannon@mydit.ie"
 __status__ = "Assignment 2 AI2"
 
-from pandas import DataFrame
-from sklearn import preprocessing
-from sklearn import tree
-from sklearn import svm
-from sklearn import cross_validation
+
 from sklearn.feature_extraction import DictVectorizer
-from sklearn import cross_validation
-from sklearn.cross_validation import train_test_split
-from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import datetime
 
+print("Starting......")
+print(datetime.datetime.now())
+       
 def main():
-        # read in trainingset data and adding headers : not sure if this is the way to assign the headers as i have hardcoded 1,2,3,4,5,6.... to stop out_of_index error
+       # read in trainingset data and adding headers : not sure if this is the way to assign the headers as i have hardcoded 1,2,3,4,5,6.... to stop out_of_index error
        columnHeadings=['Id','Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Vertical_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points','Wilderness_Area','1','2','3','Soil_Type','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','Cover_Type']
        Tdata = pd.read_csv("data/trainingset.txt",header=None,names=columnHeadings,index_col=False,na_values=['?'],nrows=435148)
        # Extract Target Feature
@@ -58,7 +54,6 @@ def main():
        cat_dfs= numeric_dfs.T.to_dict().values()
        ids = Qdata['Id']
        #q = {'Elevation':[2000,2516],'Aspect':[23,23],'Slope':[6,6],'Horizontal_Distance_To_Hydrology':[150,150],'Vertical_Distance_To_Hydrology':[4,4],'Horizontal_Distance_To_Roadways':[658,658],'Hillshade_9am':[216,216],'Hillshade_Noon':[227,227],'Hillshade_3pm':[147,147],'Horizontal_Distance_To_Fire_Points':[5541,5541]}
-       col_names = ['Elevation','Aspect','Slope','Horizontal_Distance_To_Hydrology','Vertical_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points']
        qdf = pd.DataFrame.from_dict(list(cat_dfs),orient="columns")
        #extract the numeric features
        q_num = qdf[numeric_features].as_matrix() 
